5bf49ee142215dfa7fd8219c5b9078af6ec8494b
Nicolas Salleron <nicolas.salleron@gmail.com>
2022-03-11 22:11:41 +0100

    CompetitionCubit tests -> Success

--- lib/presentation/competition/logic/competition_cubit.dart
+++ lib/presentation/competition/logic/competition_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutball/core/params/competition_request_params.dart';
+import 'package:flutball/core/resources/data_state.dart';
 import 'package:flutball/domain/entities/helpers/types_helper.dart';
 import 'package:flutball/domain/usecases/get_competitions_usecase.dart';
 import 'package:meta/meta.dart';
@@ -11,11 +12,12 @@ class CompetitionCubit extends Cubit<CompetitionState> {
 
   CompetitionCubit({
     required GetCompetitionUseCase getCompetitionUseCase,
-  })
-      : _getCompetitionUseCase = getCompetitionUseCase,
+  })  : _getCompetitionUseCase = getCompetitionUseCase,
         super(CompetitionInitial());
 
   Future<void> fetchCompetitions({CompetitionsRequestParams? params}) async {
-
+    emit(CompetitionLoading());
+    DataState<Competitions?> matchDataState = await _getCompetitionUseCase(params ?? CompetitionsRequestParams());
+    matchDataState.when((p0) => emit(CompetitionSuccess(competitions: p0!)), (p0) => emit(CompetitionFailure()));
   }
 }
