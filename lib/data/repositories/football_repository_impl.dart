51fb98189f957ef2d8b2138288c594cef8a0d90c
Nicolas Salleron <nicolas.salleron@gmail.com>
2022-03-11 15:23:21 +0100

    FootballRepository tests -> Success

--- lib/data/repositories/football_repository_impl.dart
+++ lib/data/repositories/football_repository_impl.dart
@@ -5,15 +5,21 @@ import 'package:flutball/core/params/match_request_params.dart';
 import 'package:flutball/core/params/team_request_params.dart';
 import 'package:flutball/core/resources/data_state.dart';
 import 'package:flutball/data/datasources/remote/football_api_service.dart';
+import 'package:flutball/data/models/responses/competition_response_model.dart';
+import 'package:flutball/data/models/responses/match_response_model.dart';
+import 'package:flutball/data/models/responses/team_response_model.dart';
 import 'package:flutball/data/models/team_model.dart';
+import 'package:flutball/data/repositories/extensions.dart';
 import 'package:flutball/data/repositories/football_repository.dart';
 import 'package:flutball/domain/entities/helpers/types_helper.dart';
+import 'package:retrofit/retrofit.dart';
 
 class FootballRepositoryImpl implements FootballRepository {
   FootballRepositoryImpl({
     required this.footballApiService,
     Options? footballRepositoryOptions,
-  }) : footballRepositoryOptions = footballRepositoryOptions ?? buildCacheOptions(const Duration(minutes: 5));
+  }) : footballRepositoryOptions = footballRepositoryOptions ??
+      buildCacheOptions(const Duration(minutes: 5));
 
   final FootballApiService footballApiService;
   Options footballRepositoryOptions;
@@ -21,7 +27,18 @@ class FootballRepositoryImpl implements FootballRepository {
   @override
   Future<DataState<Matches>> getMatches(MatchRequestParams params) async {
     try {
-      return DataSuccess<Matches>([]);
+      final HttpResponse<MatchResponseModel> response =
+      await footballApiService.getMatches(
+        competitionId: params.competitionId,
+        dateFrom: params.dateFrom,
+        dateTo: params.dateTo,
+        status: params.status,
+        options: footballRepositoryOptions,
+      );
+
+      return response.whenSuccessOrDefaultError<Matches>(
+            (MatchResponseModel p0) => p0.matches,
+      );
     } on DioError catch (e) {
       return DataFailed(e);
     }
@@ -30,16 +47,29 @@ class FootballRepositoryImpl implements FootballRepository {
   @override
   Future<DataState<TeamModel>> getTeams(TeamRequestParams params) async {
     try {
-      return DataSuccess<TeamModel>(TeamModel());
+      final HttpResponse<TeamResponseModel> response =
+      await footballApiService.getTeam(
+        teamId: params.teamId,
+        options: footballRepositoryOptions,
+      );
+      return response.whenSuccessOrDefaultError<TeamModel>((p0) => p0.team);
     } on DioError catch (e) {
       return DataFailed(e);
     }
   }
 
   @override
-  Future<DataState<Competitions?>> getCompetitions(CompetitionsRequestParams params) async {
+  Future<DataState<Competitions?>> getCompetitions(
+      CompetitionsRequestParams params,
+      ) async {
     try {
-      return DataSuccess<Competitions?>([]);
+      final HttpResponse<CompetitionResponseModel> response =
+      await footballApiService.getCompetitions(
+        plan: params.plan,
+        options: footballRepositoryOptions,
+      );
+      return response
+          .whenSuccessOrDefaultError<Competitions?>((p0) => p0.competitions);
     } on DioError catch (e) {
       return DataFailed(e);
     }
